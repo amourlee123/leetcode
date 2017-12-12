1023e1041d373aa226f7ff3102734832ab42e8e7
amourlee123 <amourleey@163.com>
2017-12-13 00:54:42 +0800

    add LC205

---
+++ LC205_IsomorphicStrings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def isIsomorphic(self, s, t):
+        """
+        :type s:str
+        :type t:str
+        :rtype:bool
+        """
+        sourceMap, targetMap = dict(), dict()
+        for x in range(len(s)):
+            source, target = sourceMap.get(t[x]), targetMap.get(s[x])
+            if source is None and target is None:
+                sourceMap[t[x]], targetMap[s[x]] = s[x], t[x]
+            elif target != t[x] or source != s[x]:
+                return False
+        return True
